14df31bd8bcd2a7020219c7914fc1174c68d735a
Victor Salles <vsallesa@gmail.com>
2024-02-15 23:33:12 -0300

    feat: add tutorial multiple greetings

--- hello/hello.go
+++ hello/hello.go
@@ -10,11 +10,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gutts")
+	names := []string{"Gutts", "Casca", "Gambino", "Griffith"}
+	
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
